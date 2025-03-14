b7d1f493bb95ebbb3db6952d54d86d7f092a712c
Steven Evans <kopelli+github@gmail.com>
2025-03-14 10:35:09 -0500

    update all updates

--- update-all.ps1
+++ update-all.ps1
@@ -12,8 +12,10 @@ param(
 begin {
   Import-Module -Name Chocolatey-AU
 
-  if (Test-Path $PSScriptRoot/update_vars.ps1) {
-    . $PSScriptRoot/update_vars.ps1
+  # Create this file to consistently run this script locally.
+  $envFile = Join-Path -Path $PSScriptRoot -ChildPath '.env.ps1'
+  if (Test-Path $envFile) {
+    . $envFile
   }
 
   $Options = [ordered]@{
@@ -34,47 +36,57 @@ begin {
   }
 
   $Options | Add-PluginOption -PluginName 'Report' -PluginOptions $(
-    @{
-      Type   = 'markdown'                                   #Report type: markdown or text
-      Path   = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
-      Params = @{                                          #Report parameters:
-        Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-        NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
-        UserMessage     = '[History](#update-history)'       #  Markdown, Text: Custom user message to show
-        NoIcons         = $false                            #  Markdown: don't show icon
-        IconSize        = 32                                #  Markdown: icon size
-        Title           = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
+    if ($env:au_UpdateAll_Plugin_Report -eq $true) {
+      @{
+        Type   = 'markdown'                                   #Report type: markdown or text
+        Path   = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
+        Params = @{                                          #Report parameters:
+          Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+          NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
+          UserMessage     = '[History](#update-history)'       #  Markdown, Text: Custom user message to show
+          NoIcons         = $false                            #  Markdown: don't show icon
+          IconSize        = 32                                #  Markdown: icon size
+          Title           = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
+        }
       }
     }
   )
   $Options | Add-PluginOption -PluginName 'History' -PluginOptions $(
-    @{
-      Lines           = 30                                          #Number of lines to show
-      Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
-      Path            = "$PSScriptRoot\Update-History.md"            #Path where to save history
+    if ($env:au_UpdateAll_Plugin_History -eq $true) {
+      @{
+        Lines           = 30                                          #Number of lines to show
+        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+        Path            = "$PSScriptRoot\Update-History.md"            #Path where to save history
+      }
     }
   )
   $Options | Add-PluginOption -PluginName 'Gist' -PluginOptions $(
-    @{
-      Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-      ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-      Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+    if ($env:au_UpdateAll_Plugin_Gist -eq $true) {
+      @{
+        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+      }
     }
   )
   $Options | Add-PluginOption -PluginName 'Git' -PluginOptions $(
-    @{
-      User     = ''                                       #Git username, leave empty if github api key is used
-      Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+    if ($env:au_UpdateAll_Plugin_Git -eq $true) {
+      @{
+        User     = ''                                       #Git username, leave empty if github api key is used
+        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+      }
     }
   )
   $Options | Add-PluginOption -PluginName 'RunInfo' -PluginOptions $(
-    @{
-      Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
-      Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+    if ($env:au_UpdateAll_Plugin_RunInfo -eq $true) {
+      @{
+        Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
+        Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+      }
     }
   )
   $Options | Add-PluginOption -PluginName 'Mail' -PluginOptions $(
-    if ($Env:mail_user) {
+    if ($env:au_UpdateAll_Plugin_Mail -eq $true -and $Env:mail_user) {
       @{
         To          = $Env:mail_user
         Server      = $Env:mail_server
